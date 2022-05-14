1556509c2d9c829cfbab439e48123f9aa22dc432
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-15 00:17:54 +0700

    Update preg_replace.php

--- assets/preg_replace.php
+++ assets/preg_replace.php
@@ -1,11 +1,11 @@
 <?
-		$getProfanities = $this->profanities->get("profanities");
-		$character = $this->config->get("characterReplaced");
-		foreach ($getProfanities as $profanities) {
-			$partern = "/". $profanities . "/";
-			$subject = strtolower($msg);
-			$multi = mb_strlen($profanities, "utf8");
-			$replacement = str_repeat($character, $multi);
-			$filteredMsg = preg_replace($partern, $replacement, $subject);
-			return $filteredMsg;
-		}
+$getProfanities = $this->profanities->get("profanities");
+$character = $this->config->get("characterReplaced");
+foreach ($getProfanities as $profanities) {
+	$partern = "/" . $profanities . "/";
+	$subject = strtolower($msg);
+	$multi = mb_strlen($profanities, "utf8");
+	$replacement = str_repeat($character, $multi);
+	$filteredMsg = preg_replace($partern, $replacement, $subject);
+	return $filteredMsg;
+}
