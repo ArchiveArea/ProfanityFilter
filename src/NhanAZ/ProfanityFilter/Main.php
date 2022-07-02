f5e7f3a8b8c818ff23426847fb7cde31abe5ec9c
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-07-02 21:56:21 +0700

    Skeleton

--- src/NhanAZ/ProfanityFilter/Main.php
+++ src/NhanAZ/ProfanityFilter/Main.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace NhanAZ\ProfanityFilter;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener {
 
+	protected function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onChat(PlayerChatEvent $event): void {
+		$message = $event->getMessage();
+		$profanitys = ["hello", "nhanaz"];
+		foreach ($profanitys as $profanity) {
+			$message = preg_replace("/" . $profanity . "/i", str_repeat("*", mb_strlen($profanity, "utf8")), $message);
+			$event->setMessage($message);
+		}
+	}
 }
